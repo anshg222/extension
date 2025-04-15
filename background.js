8197fddcb4d6e3ee470976c0606e84b136c7f237
anshg222 <anshg5818@gmail.com>
2025-04-15 09:00:05 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,35 @@
-const blockedSites = [
-  "bet365.com",
-  "1xbet.com",
-  "pokerstars.com",
-  "888.com",
-  "williamhill.com",
-  "betway.com",
-  "bwin.com",
-  "betfair.com",
-  "leovegas.com",
-  "draftkings.com",
-  "tiranga-games.com",
-];
-
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    const url = new URL(details.url);
-    const hostname = url.hostname.replace("www.", "");
-    console.log("Trying to access:", hostname);
-    if (blockedSites.includes(hostname)) {
-      return { cancel: true };
+[
+  {
+    "id": 1,
+    "priority": 1,
+    "action": {
+      "type": "block"
+    },
+    "condition": {
+      "urlFilter": "bet365.com",
+      "resourceTypes": ["main_frame"]
     }
   },
-  { urls: ["<all_urls>"] },
-  ["blocking"]
-);
+  {
+    "id": 2,
+    "priority": 1,
+    "action": {
+      "type": "block"
+    },
+    "condition": {
+      "urlFilter": "1xbet.com",
+      "resourceTypes": ["main_frame"]
+    }
+  },
+  {
+    "id": 3,
+    "priority": 1,
+    "action": {
+      "type": "block"
+    },
+    "condition": {
+      "urlFilter": "betway.com",
+      "resourceTypes": ["main_frame"]
+    }
+  }
+]
